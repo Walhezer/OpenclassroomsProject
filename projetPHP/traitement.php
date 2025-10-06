ed8f1920dee3cf018a43ed05f3d6b3a1ac4d1e2f
Etienne ESPIN <espinetienne@gmail.com>
2025-10-06 11:12:54 +0200

    Ajout traitement.php pour vérifier le formulaire

---
+++ projetPHP/traitement.php
@@ -0,0 +1,18 @@
+<? 
+
+if(empty($_POST['titre']) || empty($_POST['description']) || empty($_POST['artiste']) || empty($_POST['image']) 
+    || strlen($_POST['description']) < 3
+    || !filter_var($_POST['image'], FILTER_VALIDATE_URL))
+
+    
+    {
+    header('Location: ajouter.php');
+
+
+} else {
+    $titre = htmlspecialchars($_POST['titre']);
+    $description = htmlspecialchars($_POST['description']);
+    $artiste = htmlspecialchars($_POST['artiste]']);
+    $image = htmlspecialchars($_POST['image']);
+    
+}
