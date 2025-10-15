9d03eb4c3ab1e7dddee48eeba80c716d4322da7c
Etienne ESPIN <espinetienne@gmail.com>
2025-10-15 12:16:31 +0200

    affichage message d'erreur lorsque oeuvre non trouvée et redirection page d'accueil

--- oeuvre.php
+++ oeuvre.php
@@ -1,33 +1,43 @@
 <?php
+require 'bdd.php';
+$bdd = connexion();
+
+// Vérification de l'ID 
+if (empty($_GET['id'])) {
+    header('Location: index.php');
+    exit();
+}
+
+// Requête 
+$requete = $bdd->prepare('SELECT * FROM oeuvres WHERE oeuvre_id=?');
+$requete->execute([$_GET['id']]);
+$oeuvre = $requete->fetch(PDO::FETCH_ASSOC);
+
+
+// Si aucune oeuvre trouvé, on redirige vers la page d'accueil
+if (!$oeuvre) {
     require 'header.php';
-    require 'bdd.php';
-    $bdd=connexion();
+    echo "<h2 style='color:red; text-align:center; margin-top:50px;'>Cette œuvre n’existe pas ou a été supprimée.</h2>";
+    echo "<p style='text-align:center;'><a href='index.php'>← Retour à l’accueil</a></p>";
+    require 'footer.php';
+    exit;
+}
 
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
-        header('Location: index.php');
-        exit();
-    }
-    $requete = $bdd->prepare('SELECT * FROM oeuvres WHERE oeuvre_id=?');
-    $requete->execute([$_GET['id']]);
-    $oeuvre = $requete->fetch();
-
-
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
-    }
+// Si oeuvre trouvée, on affiche la page 
+require 'header.php';
 ?>
+
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= htmlspecialchars ($oeuvre['image']) ?>" 
+             alt="<?= htmlspecialchars ($oeuvre['titre']) ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?= htmlspecialchars($oeuvre['titre']) ?></h1>
+        <p class="description"><?= htmlspecialchars($oeuvre['artiste']) ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+            <?= nl2br(htmlspecialchars($oeuvre['description'])) ?>
         </p>
     </div>
 </article>
